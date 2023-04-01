b8c9ee4e2a16735d33ed236e114efb1ed9af6b82
JuctTr <1301842163@qq.com>
2023-04-01 20:57:05 +0800

    feat(🤡): optimize code

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import {
   createPxReplace,
   declarationExists,
   getUnit,
-  isExclude,
   validateParams,
+  _isArray,
+  _isRegExp,
 } from './utils';
 import objectAssign from 'object-assign';
 
 import { AtRule, Root, Rule } from 'postcss';
-import postcss from 'postcss';
 
 const defaults: Required<Omit<OptionType, 'exclude' | 'include'>> = {
   unitToConvert: 'px',
@@ -34,6 +34,60 @@ const defaults: Required<Omit<OptionType, 'exclude' | 'include'>> = {
 const ignoreNextComment = 'px-to-viewport-ignore-next';
 const ignorePrevComment = 'px-to-viewport-ignore';
 
+function excludeFiles(filePath: string, options: OptionType) {
+  const exclude = options.exclude;
+  if (!exclude) return false;
+
+  // 如果是正则表达式，且命中，那么返回 true ，表示退出，不再继续
+  // @ts-ignore
+  if (_isRegExp(exclude)) return exclude.test(filePath);
+
+  // 数组中，只要有一个命中，那么返回 true ，表示退出，不再继续
+  const result = [];
+  if (_isArray(exclude)) {
+    // @ts-ignore
+    for (let i = 0; i < exclude.length; i++) {
+      const item = exclude[i];
+      if (_isRegExp(item)) {
+        result.push(item.test(filePath));
+      } else {
+        throw new TypeError(
+          'options.exclude should be RegExp or Array of RegExp.',
+        );
+      }
+    }
+    return result.some(item => item);
+  }
+  throw new TypeError('options.exclude should be RegExp or Array of RegExp.');
+}
+
+function includeFiles(filePath: string, options: OptionType) {
+  const include = options.include;
+  if (!include) return true;
+
+  // 如果是正则表达式，且命中，那么返回 true ，表示继续
+  // @ts-ignore
+  if (_isRegExp(include)) return include.test(filePath);
+
+  // 数组中，只要有一个命中，那么返回 true ，表示继续
+  const result = [];
+  if (_isArray(include)) {
+    // @ts-ignore
+    for (let i = 0; i < include.length; i++) {
+      const item = include[i];
+      if (_isRegExp(item)) {
+        result.push(item.test(filePath));
+      } else {
+        throw new TypeError(
+          'options.include should be RegExp or Array of RegExp.',
+        );
+      }
+    }
+    return result.some(item => item);
+  }
+  throw new TypeError('options.include should be RegExp or Array of RegExp.');
+}
+
 const postcssPxToViewport = (options: OptionType) => {
   const opts = objectAssign({}, defaults, options);
 
@@ -47,23 +101,11 @@ const postcssPxToViewport = (options: OptionType) => {
       // @ts-ignore 补充类型
       css.walkRules((rule: RuleType) => {
         // Add exclude option to ignore some files like 'node_modules'
-        const file = rule.source?.input.file || '';
-        if (opts.exclude && file) {
-          if (
-            Object.prototype.toString.call(opts.exclude) === '[object RegExp]'
-          ) {
-            if (isExclude(opts.exclude as RegExp, file)) return;
-          } else if (
-            // Object.prototype.toString.call(opts.exclude) === '[object Array]' &&
-            opts.exclude instanceof Array
-          ) {
-            for (let i = 0; i < opts.exclude.length; i++) {
-              if (isExclude(opts.exclude[i], file)) return;
-            }
-          } else {
-            throw new Error('options.exclude should be RegExp or Array.');
-          }
-        }
+        const filePath = rule.source?.input.file || '';
+
+        if (!includeFiles(filePath, opts)) return;
+
+        if (excludeFiles(filePath, opts)) return;
 
         if (blacklistedSelector(opts.selectorBlackList, rule.selector)) return;
 
@@ -74,7 +116,7 @@ const postcssPxToViewport = (options: OptionType) => {
             if (!satisfyPropList(decl.prop)) return;
             let landscapeWidth;
             if (typeof opts.landscapeWidth === 'function') {
-              const num = opts.landscapeWidth(file);
+              const num = opts.landscapeWidth(filePath);
               if (!num) return;
               landscapeWidth = num;
             } else {
@@ -99,7 +141,22 @@ const postcssPxToViewport = (options: OptionType) => {
         // if (!validateParams(rule.parent?.params, opts.mediaQuery)) return;
 
         rule.walkDecls((decl, i) => {
-          if (decl.value.indexOf(opts.unitToConvert) === -1) return;
+          // 增加一个特性，如果是大写的 PX , 那么就扭转为小写的，忽略转换，保留原单位
+          const declValue = decl.value;
+          const isHaveLowerCaseUnit =
+            declValue.indexOf(opts.unitToConvert) > -1;
+          const isHaveUpperCaseUnit =
+            declValue.indexOf(opts.unitToConvert.toUpperCase()) > -1;
+
+          // 小写，大写都没有，直接退出
+          if (!isHaveLowerCaseUnit && !isHaveUpperCaseUnit) return;
+
+          // 没有小写，有大写，修改为小写，并退出
+          if (!isHaveLowerCaseUnit && isHaveUpperCaseUnit) {
+            decl.value = declValue.toLowerCase();
+            return;
+          }
+
           if (!satisfyPropList(decl.prop)) return;
 
           const prev = decl.prev();
@@ -140,7 +197,7 @@ const postcssPxToViewport = (options: OptionType) => {
             unit = opts.landscapeUnit;
 
             if (typeof opts.landscapeWidth === 'function') {
-              const num = opts.landscapeWidth(file);
+              const num = opts.landscapeWidth(filePath);
               if (!num) return;
               size = num;
             } else {
@@ -149,7 +206,7 @@ const postcssPxToViewport = (options: OptionType) => {
           } else {
             unit = getUnit(decl.prop, opts);
             if (typeof opts.viewportWidth === 'function') {
-              const num = opts.viewportWidth(file);
+              const num = opts.viewportWidth(filePath);
               if (!num) return;
               size = num;
             } else {
